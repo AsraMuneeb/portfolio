345482142b5547409383d4971e14074daa987b7c
= <=>
2024-10-02 19:43:17 +0500

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import React from 'react'; // Ensure React is imported
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';  // Import BrowserRouter
@@ -13,14 +14,5 @@ createRoot(document.getElementById('root')).render(
 );
 
 
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
 
